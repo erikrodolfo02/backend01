e2a103a6d4b59858188a458dfb12eefd623ed79c
erik <erikrodolfo23@gmail.com>
2026-05-18 23:48:07 -0300

    Projeto inicial

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/saudacao", (req, res) => {
+  const { nome } = req.body;
+
+  res.json({
+    mensagem: `Olá ${nome}!`,
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000");
+});
